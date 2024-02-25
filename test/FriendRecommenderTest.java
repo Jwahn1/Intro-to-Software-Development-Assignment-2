def6c6a3afc2b2b8b3791b5f1578325f32454b61
Jwahn1 <javierwith@gmail.com>
2024-02-25 14:55:28 -0400

    added test for friendRecommender

--- test/FriendRecommenderTest.java
+++ test/FriendRecommenderTest.java
@@ -30,8 +30,35 @@ class FriendRecommenderTest {
     assertEquals(u.name + " and " + g.name + " should be friends", al.get(0), "Incorrect recommendation");
   }
 
+  /*
+   Test checks whether Returned arraylist is empty and whether the returned message is the expected one as specified in
+   function description
+   */
   @Test
   void testMakeRecommendations() {
+    User u = new User("A");
+    User f = new User(friendNames[1]);
+    User g = new User("B");
+
+    //the final message should be "A and B should be friends"
+    f.friend(g.name);
+    u.friend(f.name);
+    
+    FriendRecommender fr = new FriendRecommender();
+    ArrayList<String> recommended = new ArrayList<String>();
+
+    fr.makeRecommendations(u,f,recommended);
+
+    //checks whether the arraylist is empty guarding the below assert
+    assertNotEquals(0,recommended.size(),"the function returns no message");
+
+    //checks whether the returned message is as expected
+    assertEquals(recommended.get(0),"A and B should be friends","" +
+    "The function does not return a message recommending the friends for A");
+
+
+
+
   }
 
   // add more tests as needed using white-box, black-box or a mix of testing strategies
